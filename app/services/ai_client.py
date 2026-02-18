446a16a0b50c35a55608f78211fefda133c03070
Mitesh Sharma <miteshsharma63114@gmail.com>
2026-02-18 08:57:29 +0530

    Restructured the backend folder

--- app/services/ai_client.py
+++
@@ -1,9 +0,0 @@
-# app/services/ai_client.py
-
-from google import genai
-from app.core.config import settings
-
-if not settings.GOOGLE_API_KEY:
-    raise ValueError("GOOGLE_API_KEY is not set in environment variables.")
-
-client = genai.Client(api_key=settings.GOOGLE_API_KEY)
